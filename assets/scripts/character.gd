bfd247083ab99dfe238cc42a64f799a98341a3ad
Owen Luts <owenlutsdev@gmail.com>
2024-10-12 19:17:15 -0400

    added new sprites, added Pandora animations

--- assets/scripts/character.gd
+++ assets/scripts/character.gd
@@ -2,28 +2,25 @@ extends CharacterBody2D
 
 
 const SPEED = 50.0
-const JUMP_VELOCITY = 100.0
-
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -1 * JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk");
 		if velocity.x > 0:
 			$AnimatedSprite2D.flip_h = false;
 		else:
 			$AnimatedSprite2D.flip_h = true;
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play('idle')
+	
+	if not is_on_floor():
+		$AnimatedSprite2D.play("fall")
+		velocity += get_gravity() * delta
+	
 
 	move_and_slide()
