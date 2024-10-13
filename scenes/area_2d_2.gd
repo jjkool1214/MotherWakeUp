3683234f93ecea3fcc7dbb898b2ac8c922cdca04
jjkool1214 <jaysenstolarz@gmail.com>
2024-10-13 12:17:51 -0400

    NEW FUNCTIONALUTY FOR INTERACTABLESSSSS

--- scenes/area_2d_2.gd
+++ scenes/area_2d_2.gd
@@ -15,9 +15,6 @@ func _process(delta):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("bfruhygf")
 	entered = true
-	
-	
-
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	print("fbryeui")
@@ -26,4 +23,7 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	
 func _input(ev):
 	if entered and Input.is_key_pressed(KEY_E):
-		get_tree().change_scene_to_file("res://scenes/HomeInterior.tscn")
+		do_something()
+
+func do_something():
+	print("bitch")
