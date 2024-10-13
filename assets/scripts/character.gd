6c9c2999d9d84d9676a5daa9b9c822c47335a7e3
jjkool1214 <jaysenstolarz@gmail.com>
2024-10-13 00:49:03 -0400

    Finished getting some stuff set up

--- assets/scripts/character.gd
+++ assets/scripts/character.gd
@@ -23,3 +23,13 @@ func _physics_process(delta):
 	
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	$"../Home/Area2D/DoorInput".text = "You can enter BITCH BOT"
+	print("shit yourself")
+	
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	$"../Home/Area2D/DoorInput".text = ""
+	print("shit yourself")
