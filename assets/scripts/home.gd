02a42ef03252120f704b137ed7a2f23465626de8
Owen Luts <oluts47@gmail.com>
2024-10-13 13:06:14 -0400

    stash

--- assets/scripts/home.gd
+++ assets/scripts/home.gd
@@ -1,12 +1,19 @@
 extends Sprite2D
 
+var can_enter = false
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	if can_enter && Input.is_action_just_pressed("interact"):
+		print('going through doorway')
 
-func _on_button_pressed():
-	var distance = $"../Player".global_position - $".".global_position
-	if (distance.x < 30):
-		get_tree().change_scene_to_file("res://scenes/HomeInterior.tscn")
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.get_class() == "CharacterBody2D":
+		print('can enter')
+		can_enter = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	can_enter = false
